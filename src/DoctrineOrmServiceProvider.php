a322060323ffc265ec39bc9c116b26c814b73c74
Luke Watts <luke@luke-watts.com>
2017-01-25 20:16:42 +0000

    Bootstrapping provider

---
+++ src/DoctrineOrmServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Affinity4\Invoice.
+ *
+ * (c) 2017 Luke Watts <luke@affinity4.ie>
+ *
+ * This software is licensed under the MIT license. For the
+ * full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Affinity4\Provider\Silex\DoctrineOrm;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $container)
+    {
+        
+    }
+}
